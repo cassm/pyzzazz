b75995ea7406c6e1255b33af286c723bcc776dcd
Cass May <ingeniousmammal@gmail.com>
2019-04-08 16:04:59 +0100

    rename receive_command() to more accurate parse_command()

--- fixture.py
+++ fixture.py
@@ -1,14 +1,12 @@
 class Fixture:
-    def __init__(self, config, sender):
+    def __init__(self, config):
         self.name = config.get("name", "No name present in fixture definition")
         self.location = config.get("location", "No location present in fixture definition")
-        self.line = config.get("line", "No line present in fixture definition")
-        self.sender = sender
 
     def send(self):
         pass
 
-    def receive_command(self, command_string):
+    def parse_command(self, command_string):
         pass
 
     def refresh(self):
