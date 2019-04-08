a7df76db76607132d43cda9438e2df6d97dc1706
Cass May <ingeniousmammal@gmail.com>
2019-04-08 20:58:29 +0100

    Initialise fixtures and senders based on config, implement update func

--- pyzzazz.py
+++ pyzzazz.py
@@ -1,7 +1,9 @@
 from configparser import ConfigParser
+from sender import Sender
 from palette import Palette
 from dodecahedron import Dodecahedron
 import signal
+import time
 
 
 class Pyzzazz:
@@ -15,11 +17,51 @@ class Pyzzazz:
         senders = self.config_parser.get_senders()
         controllers = self.config_parser.get_controllers()
 
+        self.senders = []
+        for sender_conf in self.config_parser.get_senders():
+            # check for duplicate names
+            sender_names = tuple(sender.name for sender in self.senders)
+            if sender_conf.get("name", "") in sender_names:
+                raise Exception("Pyzzazz: config specifies one or more senders with identical name {}".format(sender_conf.get("name", "")))
+
+            # check for duplicate ports
+            sender_ports = tuple(sender.port for sender in self.senders)
+            if sender_conf.get("port", "") in sender_ports:
+                raise Exception("Pyzzazz: config specifies one or more senders with identical port {}".format(sender_conf.get("port", "")))
+
+            print("Creating sender {} on port {}".format(sender_conf.get("name", ""), sender_conf.get("port", "")))
+            self.senders.append(Sender(sender_conf))
+
         self.fixtures = []
-        for fixture in self.config_parser.get_fixtures():
-            if fixture.get("geometry", "") == "dodecahedron":
-                # FIXME pass in sender here
-                self.fixtures.append(Dodecahedron(fixture, None))
+        for fixture_conf in self.config_parser.get_fixtures():
+            # check for duplicate names
+            fixture_names = tuple(fixture.name for fixture in self.fixtures)
+            if fixture_conf.get("name", "") in fixture_names:
+                raise Exception("Pyzzazz: config specifies one or more fixtures with identical name {}".format(fixture_conf.get("name", "")))
+
+            # check sender exists
+            sender_names = tuple(sender.name for sender in self.senders)
+            sender_name = fixture_conf.get("sender", "")
+            if sender_name not in sender_names:
+                raise Exception("Pyzzazz: Fixture {} specified with undefined sender {}".format(fixture_conf.get("name", ""), sender_name))
+
+            if fixture_conf.get("geometry", "") == "dodecahedron":
+                print("Creating dodecahedron {} with sender {}".format(fixture_conf.get("name", ""), fixture_conf.get("sender", "")))
+                self.fixtures.append(Dodecahedron(fixture_conf, self.senders[sender_names.index(sender_name)]))
+                break
+
+    def update(self):
+        self.effective_time += (time.time() - self.last_update) * self.speed
+
+        for sender in self.senders:
+            if not sender.is_connected():
+                sender.try_connect()
+
+
+        for fixture in self.fixtures:
+            fixture.update(self.effective_time)
+            fixture.send()
+
 
 class GracefulKiller:
     kill_now = False
