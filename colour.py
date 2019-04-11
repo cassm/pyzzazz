27b1cb1e7cfbedcc0360ce274015ad425bc9efbe
Cass May <ingeniousmammal@gmail.com>
2019-04-11 18:31:27 +0100

    Neaten up Colour class

--- colour.py
+++ colour.py
@@ -1,6 +1,6 @@
 from operator import gt, lt, eq, le, ne, ge
 class Colour:
-    def __init__(self, r=0, g=0, b=0):
+    def __init__(self, r=0.0, g=0.0, b=0.0):
         self.r = r
         self.g = g
         self.b = b
@@ -9,41 +9,118 @@ class Colour:
         self.__rsub__ = self.__sub__
 
     def __mul__(self, o):
-        return Colour(max(self.r * o, 0), max(self.g * o, 0), max(self.b * o, 0))
+        if isinstance(o, Colour):
+            return Colour(0, 0, 0).channelwise_max(Colour(self.r * o.r, self.g * o.g, self.b * o.b))
+
+        elif isinstance(o, int) or isinstance(o, float):
+            return Colour(0, 0, 0).channelwise_max(Colour(self.r * o, self.g * o, self.b * o))
+
+        else:
+            raise Exception("Must multiply Colour by either Colour, int, or float")
 
     def __imul__(self, o):
-        self.r = max(self.r * o, 0)
-        self.g = max(self.g * o, 0)
-        self.b = max(self.b * o, 0)
+        if isinstance(o, Colour):
+            self.r = max(self.r * o.r, 0)
+            self.g = max(self.g * o.g, 0)
+            self.b = max(self.b * o.b, 0)
+
+        elif isinstance(o, int) or isinstance(o, float):
+            self.r = max(self.r * o, 0)
+            self.g = max(self.g * o, 0)
+            self.b = max(self.b * o, 0)
+
+        else:
+            raise Exception("Must multiply Colour by either Colour, int, or float")
+
         return self
 
-    def __div__(self, o):
-        return Colour(max(self.r * o, 0), max(self.g * o, 0), max(self.b * o, 0))
+    def __truediv__(self, o):
+        if isinstance(o, Colour):
+            return Colour(0, 0, 0).channelwise_max(Colour(self.r / o.r, self.g / o.g, self.b / o.b))
+
+        elif isinstance(o, int) or isinstance(o, float):
+            return Colour(0, 0, 0).channelwise_max(Colour(self.r / o, self.g / o, self.b / o))
+
+        else:
+            raise Exception("Must divide Colour by either Colour, int, or float")
+
+    def __floordiv__(self, o):
+        if isinstance(o, Colour):
+            return Colour(0, 0, 0).channelwise_max(Colour(int(self.r / o.r), int(self.g / o.g), int(self.b / o.b)))
+
+        elif isinstance(o, int) or isinstance(o, float):
+            return Colour(0, 0, 0).channelwise_max(Colour(int(self.r / o), int(self.g / o), int(self.b / o)))
+
+        else:
+            raise Exception("Must divide Colour by either Colour, int, or float")
 
     def __idiv__(self, o):
-        self.r = max(self.r / o, 0)
-        self.g = max(self.g / o, 0)
-        self.b = max(self.b / o, 0)
+        if isinstance(o, Colour):
+            self.r = max(self.r / o.r, 0)
+            self.g = max(self.g / o.g, 0)
+            self.b = max(self.b / o.b, 0)
+
+        elif isinstance(o, int) or isinstance(o, float):
+            self.r = max(self.r / o, 0)
+            self.g = max(self.g / o, 0)
+            self.b = max(self.b / o, 0)
+
+        else:
+            raise Exception("Must divide Colour by either Colour, int, or float")
+
         return self
 
     def __add__(self, o):
-        return Colour(max(self.r + o.r, 0), max(self.g + o.g, 0), max(self.b + o.b, 0))
+        if isinstance(o, Colour):
+            return Colour(0, 0, 0).channelwise_max(Colour(self.r + o.r, self.g + o.g, self.b + o.b))
+
+        elif isinstance(o, int) or isinstance(o, float):
+            return Colour(0, 0, 0).channelwise_max(Colour(self.r + o, self.g + o, self.b + o))
+
+        else:
+            raise Exception("Must add Colour to either Colour, int, or float")
 
     def __iadd__(self, o):
-        self.r = max(self.r + o.r, 0)
-        self.g = max(self.g + o.g, 0)
-        self.b = max(self.b + o.b, 0)
+        if isinstance(o, Colour):
+            self.r = max(self.r + o.r, 0)
+            self.g = max(self.g + o.g, 0)
+            self.b = max(self.b + o.b, 0)
+
+        elif isinstance(o, int) or isinstance(o, float):
+            self.r = max(self.r + o, 0)
+            self.g = max(self.g + o, 0)
+            self.b = max(self.b + o, 0)
+
+        else:
+            raise Exception("Must add Colour to either Colour, int, or float")
+
         return self
 
     def __sub__(self, o):
-        return Colour(max(self.r - o.r, 0), max(self.g - o.g, 0), max(self.b - o.b, 0))
+        if isinstance(o, Colour):
+            return Colour(0, 0, 0).channelwise_max(Colour(self.r - o.r, self.g - o.g, self.b - o.b))
+
+        elif isinstance(o, int) or isinstance(o, float):
+            return Colour(0, 0, 0).channelwise_max(Colour(self.r - o, self.g - o, self.b - o))
+
+        else:
+            raise Exception("Must subtract either Colour, int, or float from Colour")
 
     def __isub__(self, o):
-        self.r = max(self.r - o.r, 0)
-        self.g = max(self.g - o.g, 0)
-        self.b = max(self.b - o.b, 0)
-        return self
+        if isinstance(o, Colour):
+            self.r = max(self.r - o.r, 0)
+            self.g = max(self.g - o.g, 0)
+            self.b = max(self.b - o.b, 0)
 
+        elif isinstance(o, int) or isinstance(o, float):
+            self.r = max(self.r - o, 0)
+            self.g = max(self.g - o, 0)
+            self.b = max(self.b - o, 0)
+
+        else:
+            raise Exception("Must subtract either Colour, int, or float from Colour")
+
+        return self
 
     def channelwise_max(self, o):
         return Colour(max(self.r, o.r), max(self.g, o.g), max(self.b, o.b))
@@ -65,11 +142,11 @@ class Colour:
         return iter([self.r, self.g, self.b])
 
     def eq(self, o):
-        assert type(o) == Colour, 'must compare Colour to Colour'
+        assert isinstance(o, Colour), 'must compare Colour to Colour'
         return self.r > o.r and self.g > o.g and self.b > o.b
 
     def ne(self, o):
-        assert type(o) == Colour, 'must compare Colour to Colour'
+        assert isinstance(o, Colour), 'must compare Colour to Colour'
         return self.r != o.r and self.g != o.g and self.b != o.b
     
     def __repr__(self):
