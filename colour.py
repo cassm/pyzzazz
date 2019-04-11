2889336a4780accae5198fb5ccb10fb190f86add
pixelherd <6398412+pixelherd@users.noreply.github.com>
2019-04-11 09:18:55 +0100

    added Colour comparisons (required for Colour.max & Colour.min)

--- colour.py
+++ colour.py
@@ -1,3 +1,4 @@
+from operator import gt, lt, eq, le, ne, ge
 class Colour:
     def __init__(self, r=0, g=0, b=0):
         self.r = r
@@ -44,12 +45,36 @@ class Colour:
         return self
 
     def max(self, o):
+        assert type(o) == Colour, 'must compare Colour to Colour'
         return Colour(max(self.r, o.r), max(self.g, o.g), max(self.b, o.b))
 
     def min(self, o):
         return Colour(min(self.r, o.r), min(self.g, o.g), min(self.b, o.b))
 
+    #FIXME returns "Colour object is not iterable"
     def list(self):
         return [self.r, self.g, self.b]
 
+    def gt(self, o):
+        assert type(o) == Colour, 'must compare Colour to Colour'
+        return self.r > o.r and self.g > o.g and self.b > o.b
 
+    def lt(self, o):
+        assert type(o) == Colour, 'must compare Colour to Colour'
+        return self.r < o.r and self.g < o.g and self.b < o.b
+
+    def eq(self, o):
+        assert type(o) == Colour, 'must compare Colour to Colour'
+        return self.r > o.r and self.g > o.g and self.b > o.b
+
+    def ge(self, o):
+        assert type(o) == Colour, 'must compare Colour to Colour'
+        return self.r >= o.r and self.g >= o.g and self.b >= o.b
+
+    def le(self, o):
+        assert type(o) == Colour, 'must compare Colour to Colour'
+        return self.r <= o.r and self.g <= o.g and self.b <= o.b
+
+    def ne(self, o):
+        assert type(o) == Colour, 'must compare Colour to Colour'
+        return self.r != o.r and self.g != o.g and self.b != o.b
