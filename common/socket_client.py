f189ab4f31f37f64fab38f4df0097697341002fe
Cass May <ingeniousmammal@gmail.com>
2019-04-12 21:38:10 +0100

    Flesh out socket client class partially

--- common/socket_client.py
+++ common/socket_client.py
@@ -1,27 +1,76 @@
+import errno
 import socket
+import select
+from common.packet_handler import CommPacketHandler
+from common.packet_handler import CommHeader
+from common.packet_handler import NameResponsePayload
 
 
 class SocketClient:
-    def __init__(self, port, host='127.0.0.1'):
+    def __init__(self, name, port, host='127.0.0.1'):
+        self.name = name
+        self._packet_handler = CommPacketHandler()
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # now connect to the web server on port 80
-        # - the normal http port
-        self._socket.connect((host, port))
+        self._outbound_byte_buffer = bytearray()
+
         print("SocketClient: connecting on {}:{}".format(host, port))
+        # FIXME make this sensible
+        self._socket.connect((host, port))
+        print("SocketClient: connected on {}:{}".format(host, port))
+        self._inout = [self._socket]
+
+    def poll(self):
+        readable, writeable, errored = select.select(list(self._inout),
+                                                     list(self._inout),
+                                                     list(self._inout),
+                                                     0.5)
+
+        if len(readable) > 0:
+            self._packet_handler.add_bytes(self._socket.recv(4096))
+
+        if len(writeable) > 0:
+                if len(self._outbound_byte_buffer) > 0:
+                    try:
+                        print("sending bytes: {}".format(self._outbound_byte_buffer))
+                        sent = self._socket.send(self._outbound_byte_buffer)
+                        self._outbound_byte_buffer = self._outbound_byte_buffer[sent:]
+                        print("Sent {} bytes".format(sent))
+
+                    except socket.error as e:
+                        if e.errno != errno.EAGAIN:
+                            self._socket.close()
+                            # FIXME uuhhhh
+                            print("woopsie daisy")
+                            raise e
+
+                        print('Blocking with', len(self._outbound_byte_buffer), 'remaining')
+
+        if len(errored) > 0:
+            self._socket.close()
+            # FIXME uuhhhh
+            raise Exception("woopsie daisy")
+
+        self.process_received_packets()
+
+    def process_received_packets(self):
+        for packet in self._packet_handler.available_packets:
+            if packet["msgtype"] == "name_request":
+                print("SocketClient: name request received")
+                payload = NameResponsePayload(name=self.name)
+                header = CommHeader(msgtype="name_reply", payload_len=len(payload.get_bytes()))
+
+                self.send_bytes(header.get_bytes() + payload.get_bytes())
+
+                self._packet_handler.available_packets.remove(packet)
+
+    def send_bytes(self, bytes):
+        if len(self._outbound_byte_buffer) == 0:
+            self._outbound_byte_buffer = bytes
+        else:
+            self._outbound_byte_buffer.extend(bytes)
 
     def get_bytes(self):
-        return self._socket.recv(4096)
+        self._packet_handler.add_bytes(self._socket.recv(4096))
 
     def close(self):
         self._socket.close()
-
-
-sock = SocketClient(48945)
-
-try:
-    while True:
-        bytes_recvd = sock.get_bytes()
-        if len(bytes_recvd) > 0:
-            print(bytes_recvd)
-finally:
-    sock.close()
