1593a8cff74eb7197d98ddb2d903be1ee0780d72
Cass May <ingeniousmammal@gmail.com>
2019-04-17 19:11:31 +0100

    Rotate mappings to face centre in cylinder

--- fixtures/cylinder.py
+++ fixtures/cylinder.py
@@ -21,11 +21,25 @@ class Cylinder(LedFixture):
 
         LedFixture.__init__(self, config, senders, overlay_handler, video_handler)
 
+        angle_from_centre = fixture_origin.to_cylindrical().theta
+
+        if fixture_origin.x < 0:
+            angle_from_centre += math.pi
+
+        print(fixture_origin)
+        print("angle from centre {}".format(angle_from_centre))
+
         for i in range(num_pixels):
             theta = (i * theta_per_pixel) % (2*math.pi)
             z = i * z_per_pixel - height/2
             led_local_cylindrical = Cylindrical(r=radius, theta=theta, z=z)
-            flat_mapping=[theta / (2.0*math.pi), i * z_per_pixel / height]
+
+            mapping_angle = theta - angle_from_centre
+            while mapping_angle < 0:
+                mapping_angle += 2*math.pi
+            mapping_angle %= (2*math.pi)
+
+            flat_mapping=[mapping_angle / (2.0*math.pi), i * z_per_pixel / height]
             led_coord = Coordinate(local_origin=fixture_origin, local_cylindrical=led_local_cylindrical)
             self.leds.append(Led(led_coord, flat_mapping=flat_mapping))
 
