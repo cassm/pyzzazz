a441f5ff76632bc801ff92110dd8547707f0fec5
Cass May <ingeniousmammal@gmail.com>
2019-04-17 20:02:29 +0100

    Clean up video handler

--- common/video_handler.py
+++ common/video_handler.py
@@ -1,35 +1,38 @@
 import os
 import cv2
 
+
 class VideoHandler:
     def __init__(self, video_path):
-        print(cv2.__version__)
-
         if not os.path.isdir(video_path):
             raise Exception("ERROR: the directory {} does not exist".format(video_path))
 
-        self.videos = []
+        self.videos = dict()
 
         for video_name in os.listdir(video_path):
             if not video_name.startswith("."):
                 file_path = os.path.join(video_path, video_name)
-                self.videos.append((video_name, file_path))
-
-        self.current_video = self.videos[0][1]
-
-        print(self.videos)
-
-        self.vidcap = cv2.VideoCapture(self.current_video)
-        success, self.frame = self.vidcap.read()
-
-        if not success:
-            raise Exception("WAAT")
+                self.videos[video_name] = file_path
 
         self.scaling_factor = 0
 
         self.width_offset = 0
         self.height_offset = 0
         self.vid_length = 1
+
+        self._switch_to_video(list(self.videos.keys())[0])
+
+    def _switch_to_video(self, name):
+        if name not in self.videos.keys():
+            raise Exception("Unknown video ", name)
+
+        self.current_video = self.videos[name]
+        self.vidcap = cv2.VideoCapture(self.current_video)
+
+        self.vidcap.set(cv2.CAP_PROP_POS_AVI_RATIO, 1)
+        self.vid_length = self.vidcap.get(cv2.CAP_PROP_POS_MSEC)
+
+        self._update_sampling_factors()
 
     def _update_sampling_factors(self):
         width = self.vidcap.get(3)
@@ -47,33 +50,24 @@ class VideoHandler:
 
         self.scaling_factor = min(width, height) - 1
 
-        self.vidcap.set(cv2.CAP_PROP_POS_AVI_RATIO, 1)
-        self.vid_length = self.vidcap.get(cv2.CAP_PROP_POS_MSEC)
-
     def update(self, time):
-        self._update_sampling_factors()
-
         self.vidcap.set(cv2.CAP_PROP_POS_MSEC, (time*1000) % self.vid_length)
 
         success, image = self.vidcap.read()
 
         if success:
             self.frame = image
-        # if not success:
-        #     raise Exception("WAAaaaT")
+        if not success:
+            raise Exception("Failed to read video frame")
 
     def receive_command(self, command):
-        if command["type"] == "video":
-            name = command["name"]
-            if name not in self.videos:
-                raise Exception("Unknown video file ", name)
+        if command["type"] == "video" and command["name"] == "map_video":
+            name = command["video_name"]
+
+            self._switch_to_video(name)
 
     def sample(self, flat_mapping):
         mapped_x = int(flat_mapping[0]*self.scaling_factor + self.width_offset)
         mapped_y = int(flat_mapping[1]*self.scaling_factor + self.height_offset)
 
-        try:
-            return list(self.frame[mapped_y, mapped_x])
-        except IndexError as e:
-            print("{} {} {}".format(flat_mapping, self.width_offset, self.height_offset))
-            raise e
+        return list(self.frame[mapped_y, mapped_x])
