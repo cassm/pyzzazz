5020e71906e8074e1e75adcfdda031b065f5742d
Cass May <ingeniousmammal@gmail.com>
2019-05-10 23:16:24 +0100

    Tune spark shower

--- overlays/spark_shower.py
+++ overlays/spark_shower.py
@@ -7,9 +7,9 @@ import time
 class SparkShower(Overlay):
     def __init__(self, args):
         self.last_sparkles = {}
-        self._sparkle_probability = args.get("sparkle_probability", 0.2)
+        self._sparkle_probability = args.get("sparkle_probability", 0.025)
         self.deltas = {}
-        self.global_z = {}
+        self.colour_factors = {}
 
     def get_overlaid_colours(self, colours, leds, time_since_start, fixture_name):
         # TODO cache colours and calc mode over all fixtures
@@ -17,17 +17,18 @@ class SparkShower(Overlay):
 
         if fixture_name not in self.last_sparkles.keys():
             self.deltas[fixture_name] = np.array(list(led.coord.get_delta("global") for led in leds))
-            self.global_z[fixture_name] = np.array(list(led.coord.get("global", "cartesian").z for led in leds))
             self.last_sparkles[fixture_name] = np.zeros(len(leds))
+            self.colour_factors[fixture_name] = np.random.normal(1.0, 0.3, (len(leds), 3))
 
         # determine sparkle front probability
         front_speed = 1.0
-        front_progress = front_speed * time_since_start
+        head_start = 0.5
+        front_progress = front_speed * time_since_start + head_start
 
         time_since_front = front_progress - self.deltas[fixture_name]
         time_since_front = np.maximum(1, time_since_front)
 
-        front_intensity = 1.0 / (time_since_front * 3)
+        front_intensity = 1.0 / (time_since_front * 0.5)
 
         front_intensity[self.deltas[fixture_name] > front_progress] = 0
 
@@ -38,14 +39,15 @@ class SparkShower(Overlay):
             if random.random() < faded_probability[i]:
                 self.last_sparkles[fixture_name][i] = time.time()
 
-        time_since_sparkles = np.maximum(0.5, time.time() - self.last_sparkles[fixture_name])
-        sparkle_intensities = 1.0 / np.maximum(1.0, time_since_sparkles / 1)
+        time_since_sparkles = np.maximum(1.0, time.time() - self.last_sparkles[fixture_name])
+        sparkle_intensities = 1.0 / np.maximum(0.5, time_since_sparkles / 1.5)
+        sparkle_intensities *= (1.0 / np.maximum(1.0, time_since_front / 1))
 
-
-        sparkle_intensities *= (1.0 / max(1.0, time_since_start / 8))
-
-        sparkle_colours = np.array(list([246, 101 + 101 * (1.0 / max(1.0, time_since_front[i] / 2)), 74] for i in range(len(time_since_front))))
+        sparkle_colours = np.array(list([246, 101 + 101 * (1.0 / max(1.0, time_since_front[i] / 2)), 74] for i in range(len(leds))))
+        sparkle_colours *= self.colour_factors[fixture_name]
         sparkle_colours *= sparkle_intensities[:,np.newaxis]
+
+        np.clip(255, 0, sparkle_colours)
 
         sparkle_colours = sparkle_colours[:len(colours)]
 
