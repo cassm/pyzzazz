43f6fc2f731cf0465db97432e4e7e9b418678913
Cass May <cass@cassm.net>
2022-02-10 15:34:42 +0000

    set up three.js skeleton

--- webserver/flaskr/static/script.js
+++ webserver/flaskr/static/script.js
@@ -1,5 +1,26 @@
-
-import * as THREE from 'https://cdn.skypack.dev/three@0.137.5';
+const THREE = window.THREE;
 
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
 
+renderer.setSize( window.innerWidth*0.99, window.innerHeight*0.98 );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+controls.update();
+
+function animate() {
+    requestAnimationFrame( animate );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.02;
+    controls.update();
+    renderer.render( scene, camera );
+}
+animate();
