391866eb5fc72b06d8c459261f51ad285b232e25
Cass May <cass@cassm.net>
2022-02-22 12:44:47 +0000

    store pattern names in snake case

--- handlers/pattern_handler.py
+++ handlers/pattern_handler.py
@@ -1,3 +1,4 @@
+import re
 import inspect
 import patterns
 from patterns.pattern import Pattern
@@ -7,9 +8,14 @@ class PatternHandler:
     def __init__(self):
         self.patterns = {}
 
+        # find all submodules of pattern module and store them by name
         submodules = [obj for name, obj in inspect.getmembers(patterns) if inspect.ismodule(obj)]
         for submodule in submodules:
             for name, obj in inspect.getmembers(submodule):
                 if inspect.isclass(obj) and issubclass(obj, Pattern):
                     if obj != Pattern:
-                        self.patterns[name.lower()] = obj
+                        # convert camelcase class name to snakecase identifier
+                        snake_case_name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+                        snake_case_name = re.sub('([a-z0-9])([A-Z])', r'\1_\2', snake_case_name)
+                        snake_case_name = snake_case_name.lower()
+                        self.patterns[snake_case_name] = obj
