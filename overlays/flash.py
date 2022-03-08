c1e0e812834ae1da3d9e112c43dfbbf54b047883
Cass May <cass@cassm.net>
2022-03-08 10:28:12 +0000

    allow flash overlay to be passed argless

--- overlays/flash.py
+++ overlays/flash.py
@@ -4,7 +4,7 @@ from common.utils import nonzero
 
 class Flash(Overlay):
     def __init__(self, args):
-        self.decay_factor = args["decay_factor"]
+        self.decay_factor = args.get('decay_factor', 1.0)
 
     def get_overlaid_colours(self, colours, leds, time_since_start, fixture_name):
         factor = min(2.0, 1.0 / nonzero((time_since_start + 0.25) * self.decay_factor))
